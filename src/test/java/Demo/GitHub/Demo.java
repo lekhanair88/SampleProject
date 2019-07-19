41cdfaa362e892e82dea74b7ffee2f65913fdb67
lekha.k.nair <lekha.k.nair@CDC2-L-7N87QQ2.dir.svc.accenture.com>
2019-07-19 11:23:00 +0530

    Demo Project is pushed to GitHUb !!

---
+++ src/test/java/Demo/GitHub/Demo.java
@@ -0,0 +1,10 @@
+package Demo.GitHub;
+
+import org.testng.annotations.Test;
+
+public class Demo {
+  @Test
+  public void f() {
+	  System.out.println("This Project is pushed to GitHub!!");
+  }
+}
